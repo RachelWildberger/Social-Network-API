67816384fa88224fa5c8ff0e2ee8187dfc80da02
Rachel L Wildberger <rachellwildberger@Rachels-MacBook-Pro.local>
2023-03-21 15:11:00 -1000

    file structure, created models

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now(),
+      },
+    reaction: [reactionSchema],
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length;
+  });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
